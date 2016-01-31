5c4f759b1988eeba09f2d9b7ca4c05a637372e3b
Travis Johnston <tjohnst1@gmail.com>
2016-01-31 10:59:09 -0800

    create nav component

--- app/src/js/container/App.js
+++ app/src/js/container/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'
 import Header from '../components/Header'
+import Nav from '../components/Nav'
 
 export default class App extends Component {
 
   render(){
+    const categories = ["About", "Contact", "Projects"]
     return (
       <div className="main-container">
         <Header />
+        <Nav categories={categories}/>
         <p>Hello World!</p>
       </div>
     )
