f8c49e7438a9c4cc2405204efbcc01366352de8c
Travis Johnston <tjohnst1@gmail.com>
2016-01-30 23:27:45 -0800

    initial react config (i.e. hello world test)

---
+++ app/src/js/container/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class App extends Component {
+
+  render(){
+    return (
+      <div>
+        <p>Hello World!</p>
+      </div>
+    )
+  }
+
+}
