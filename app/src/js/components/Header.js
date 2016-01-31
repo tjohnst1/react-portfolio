44b2f43df8b4037ac213504ab4031fec37970d01
Travis Johnston <tjohnst1@gmail.com>
2016-01-31 00:01:47 -0800

    create basic header component; install bourbon/neat

---
+++ app/src/js/components/Header.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+export default class Header extends Component {
+  render(){
+    return (
+      <div>
+        <h1>Travis Johnston</h1>
+        <h2>Web Developer & Designer</h2>
+      </div>
+    )
+  }
+}
