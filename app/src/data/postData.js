fa3aadc20e661e8e59f6f3cb6c62f9debe205f81
Travis Johnston <tjohnst1@gmail.com>
2016-01-31 19:01:03 -0800

    import post data; add styling to project component

---
+++ app/src/data/postData.js
@@ -0,0 +1,42 @@
+export default [
+{
+  title:  "A Cart Apart",
+  thumbnail: "/images/acartapart.jpg",
+  images: "/images/a-cart-apart.png",
+  languages: ["JavaScript", "Ruby", "HTML", "CSS"],
+  libraries: ["Google Maps API", "jQuery", "VelocityJS", "rSpec", "PhantomJS"],
+  github: "https://github.com/tjohnst1/A_Cart_Apart",
+  link: "https://a-cart-apart.herokuapp.com/",
+  browserSupport: "TBD"
+},
+{
+  title: "Tic Tac Toe",
+  thumbnail: "/images/tictactoe.jpg",
+  images: "/images/TicTacToe.png",
+  languages: ["JavaScript", "HTML", "CSS"],
+  libraries: ["jQuery", "Mocha", "Chai"],
+  github: "https://github.com/tjohnst1/TicTacToe",
+  link: "https://fathomless-forest-5686.herokuapp.com/",
+  browserSupport: "TBD"
+},
+{
+  title:  "Calorie Counter",
+  thumbnail: "/images/caloriecounter.jpg",
+  images: "/images/calorie-counter.png",
+  languages: ["Ruby", "JavaScript", "HTML", "CSS"],
+  libraries: ["Ruby on Rails", "jQuery"],
+  github: "https://github.com/tjohnst1/health-tracker",
+  link: "https://sheltered-anchorage-2234.herokuapp.com/",
+  browserSupport: "TBD"
+},
+{
+  title:  "Xenia",
+  thumbnail: "/images/xenia.jpeg",
+  images: "/images/Xenia.png",
+  languages: ["Ruby", "JavaScript"],
+  libraries: ["Sinatra", "jQuery", "RSpec", "Capybara", "Postgres", "Rake", "Active Record"],
+  github: "https://github.com/afr114/shelter_resources",
+  link: "https://xenia.herokuapp.com",
+  browserSupport: "TBD"
+}
+]
