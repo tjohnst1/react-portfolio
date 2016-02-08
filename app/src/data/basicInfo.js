378042a7cb1b34f37b617ee52a33a2589573f834
Travis Johnston <tjohnst1@gmail.com>
2016-02-08 11:47:17 -0800

    add hover effects to contact links; update basic info with shortened urls

--- app/src/data/basicInfo.js
+++ app/src/data/basicInfo.js
@@ -1,7 +1,9 @@
 const basicInfo = {
   url: "http://travisjohnston.info",
   linkedin: "https://www.linkedin.com/in/tjohnst1",
+  linkedinShort: "linkedin.com/in/tjohnst1",
   github: "https://www.github.com/tjohnst1",
+  githubShort: "github.com/tjohnst1",
   email: "tjohnst1@gmail.com"
 }
 
--- app/src/js/components/Contact.js
+++ app/src/js/components/Contact.js
@@ -15,9 +15,9 @@ export default class Contact extends Component {
       <div className={containerClasses}>
         <p className="contact-blurb">Do you have a project idea or just want to say hi? Feel free to reach out via e-mail or social media. I&#8217;ll try my best to get back to you promptly.</p>
         <ul>
-          <li><a href={basicInfo.email}><Isvg src="/images/email.svg">E-mail: </Isvg>{basicInfo.email}</a></li>
-          <li><a href={basicInfo.linkedin}><Isvg src="/images/linkedin.svg">LinkedIn: </Isvg>{basicInfo.linkedin}</a></li>
-          <li><a href={basicInfo.github}><Isvg src="/images/github.svg">Github: </Isvg>{basicInfo.github}</a></li>
+          <li><a className="contact-links" href={`mailto:${basicInfo.email}?Subject=Hello!`}><Isvg src="/images/email.svg">E-mail: </Isvg>{basicInfo.email}</a></li>
+          <li><a className="contact-links" href={basicInfo.linkedin}><Isvg src="/images/linkedin.svg">LinkedIn: </Isvg>{basicInfo.linkedinShort}</a></li>
+          <li><a className="contact-links" href={basicInfo.github}><Isvg src="/images/github.svg">Github: </Isvg>{basicInfo.githubShort}</a></li>
         </ul>
       </div>
     )
