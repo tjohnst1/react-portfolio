655068edf1809cf2b18d14eddee0fcae399d3338
Travis Johnston <tjohnst1@gmail.com>
2016-02-07 17:25:34 -0800

    add styling to about component

--- app/src/js/components/About.js
+++ app/src/js/components/About.js
@@ -8,14 +8,19 @@ export default class About extends Component {
     const containerClasses = classNames({
       "fixed-container": true,
       "offset": true,
-      "closed": this.props.navShowing
+      "closed": this.props.navShowing,
+      "about-container": true
     })
     return (
       <div className={containerClasses}>
-        <p>I am front end web developer with previous experience in graphic design and video post production. My interests, among others, include design, technology, and about how to express ideas creatively in the digital space.</p>
-        <h3>Skills</h3>
-          <p>Web Frameworks/Libraries/Applications: Ember.js, Gulp, Grunt, JavaScript, jQuery, React, Ruby, Ruby on Rails</p>
-          <p>Applications: After Effects, Atom, Compressor, Excel, Final Cut Pro, Illustrator, InDesign, Photoshop, PowerPoint, Premiere</p>
+        <p className="about-summary">I am front end web developer with previous experience in graphic design and video post production. My interests, among others, include design, technology, and about how to express ideas creatively in the digital space.</p>
+        <div className="about-skills-container">
+          <h3 className="about-skill-headline main">Skills</h3>
+          <h4 className="about-skill-headline">Programming Languages/Frameworks/Libraries</h4>
+          <p className="about-skill-set">Ember.js, Gulp, Grunt, JavaScript, jQuery, React, Ruby, Ruby on Rails</p>
+          <h4 className="about-skill-headline">Applications</h4>
+          <p className="about-skill-set">After Effects, Atom, Compressor, Excel, Final Cut Pro, Illustrator, InDesign, Photoshop, PowerPoint, Premiere</p>
+        </div>
       </div>
     )
   }
