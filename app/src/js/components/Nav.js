84d533e2c2bc799cc38b7af13e721a7c00f362a3
Travis Johnston <tjohnst1@gmail.com>
2016-02-16 18:32:56 -0800

    add down arrow

---
+++ app/src/js/components/Nav.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react'
+import classNames from 'classnames'
+
+export default class Header extends Component {
+  render(){
+    const navMenuClasses = classNames({"nav-open": this.props.navOpen, "nav-container": true})
+    return (
+      <div className={navMenuClasses}>
+        <nav onClick={this.props.toggleNav}>
+          <ul>
+            <li className="nav-link"><a href="/something">Projects</a></li>
+            <li className="nav-link"><a href="/something-else">About</a></li>
+            <li className="nav-link"><a href="#">Contact</a></li>
+          </ul>
+          <div className="nav-contact">
+            <p>Travis Johnston</p>
+            <p>tjohnst1@gmail.com</p>
+          </div>
+        </nav>
+      </div>
+    )
+  }
+}
