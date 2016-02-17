6852ce3759ffed7cc0c58d9409218d63e8ac8276
Travis Johnston <tjohnst1@gmail.com>
2016-02-16 18:32:42 -0800

    reconfigure home component styling

--- app/src/js/components/Home.js
+++ app/src/js/components/Home.js
@@ -2,12 +2,15 @@ import React, {Component, PropTypes} from 'react'
 import { Link } from 'react-router'
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
 import * as _ from 'lodash'
+import Isvg from 'react-inlinesvg'
 
 export default class Home extends Component {
   render(){
     return (
-        <div className="home-container transition container">
-          <p className="home-intro-text"><Link className="underline" to="/about">Travis</Link> is a <Link className="underline" to="/projects">front end developer</Link> currently based in Portland, Oregon.</p>
+        <div className="home-container">
+          <p className="home-intro-headline">Travis Johnston</p>
+          <p className="home-intro-deck">Web Developer & Designer</p>
+          <Isvg src="/images/down-arrow.svg">Learn More</Isvg>
         </div>
     )
   }
