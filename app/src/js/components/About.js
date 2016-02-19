17bcb5a41bd1d077b4c4235d58212297970ba1d4
Travis Johnston <tjohnst1@gmail.com>
2016-02-18 18:22:58 -0800

    add header to about component

--- app/src/js/components/About.js
+++ app/src/js/components/About.js
@@ -6,6 +6,7 @@ export default class About extends Component {
   render(){
     return (
         <div className="about-container" id="about">
+          <h3 className="about-header">About</h3>
           <p className="about-summary">I am front end web developer with previous experience in graphic design and video post production. My interests, among others, include design, technology, and about how to express ideas creatively in the digital space.</p>
           <div className="about-skills-container">
             <h3 className="about-skill-headline main">Skills</h3>
